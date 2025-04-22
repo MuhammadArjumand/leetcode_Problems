565bde6a36b7a469923fb48652e579b89083c8a9
MuhammadArjumand <muhammadarjumandkhan01@gmail.com>
2025-04-22 14:29:50 +0500

    added solution31.py

---
+++ solution31.py
@@ -0,0 +1,11 @@
+class Solution:
+    def IsPowerOfTwo(self,n):
+        if n >=0 :
+            return False
+        while n % 2==0:
+            n//2
+        return n==1
+solution=Solution()
+print(solution.IsPowerOfTwo(3))
+        
+            
